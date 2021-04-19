b6ce158e1c27f755d4ef9b3142b41eacf38bd328
soilyhouna <bigsalif@gmail.com>
2021-04-19 12:37:58 +0000

    add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
        $this->call([
             //ProduitSeeeder::class,
             CategorySeeder::class,
+            RoleSeeder::class,
         ]);
         
 
